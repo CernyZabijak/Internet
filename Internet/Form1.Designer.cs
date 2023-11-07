a79a40968322b479efbc548ba89031cb76fc47d2
Kryštof Studnička <it-krystofstudnicka@stredniskola.net>
2023-11-07 23:50:25 +0100

    uwu

--- Internet/Form1.Designer.cs
+++ Internet/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Internet
 {
-    partial class Form1
+    partial class S
     {
         /// <summary>
         ///  Required designer variable.
@@ -37,105 +37,182 @@
             label4 = new Label();
             fileSizeTextBox = new TextBox();
             CalculateButton = new Button();
-            textBox1 = new TextBox();
-            progressBar1 = new ProgressBar();
+            summaryTextbox = new TextBox();
+            StartBenchmarkButton = new Button();
+            resultTextbox = new ListBox();
+            textBox2 = new TextBox();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            comboBox1 = new ComboBox();
             SuspendLayout();
             // 
             // speedTextBox
             // 
-            speedTextBox.Location = new Point(12, 32);
+            speedTextBox.Location = new Point(14, 43);
+            speedTextBox.Margin = new Padding(3, 4, 3, 4);
             speedTextBox.Name = "speedTextBox";
-            speedTextBox.Size = new Size(100, 23);
+            speedTextBox.Size = new Size(114, 27);
             speedTextBox.TabIndex = 0;
             // 
             // latencyTextBox
             // 
-            latencyTextBox.Location = new Point(118, 32);
+            latencyTextBox.Location = new Point(135, 43);
+            latencyTextBox.Margin = new Padding(3, 4, 3, 4);
             latencyTextBox.Name = "latencyTextBox";
-            latencyTextBox.Size = new Size(100, 23);
+            latencyTextBox.Size = new Size(114, 27);
             latencyTextBox.TabIndex = 1;
             // 
             // providerTextBox
             // 
-            providerTextBox.Location = new Point(224, 32);
+            providerTextBox.Location = new Point(256, 43);
+            providerTextBox.Margin = new Padding(3, 4, 3, 4);
             providerTextBox.Name = "providerTextBox";
-            providerTextBox.Size = new Size(100, 23);
+            providerTextBox.Size = new Size(114, 27);
             providerTextBox.TabIndex = 2;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 9);
+            label1.Location = new Point(14, 12);
             label1.Name = "label1";
-            label1.Size = new Size(39, 15);
+            label1.Size = new Size(92, 20);
             label1.TabIndex = 3;
-            label1.Text = "Speed";
+            label1.Text = "Speed Mbps";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(118, 9);
+            label2.Location = new Point(135, 12);
             label2.Name = "label2";
-            label2.Size = new Size(48, 15);
+            label2.Size = new Size(82, 20);
             label2.TabIndex = 4;
-            label2.Text = "Latency";
+            label2.Text = "Latency ms";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(224, 9);
+            label3.Location = new Point(256, 12);
             label3.Name = "label3";
-            label3.Size = new Size(51, 15);
+            label3.Size = new Size(64, 20);
             label3.TabIndex = 5;
             label3.Text = "Provider";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(12, 70);
+            label4.Location = new Point(12, 84);
             label4.Name = "label4";
-            label4.Size = new Size(48, 15);
+            label4.Size = new Size(63, 20);
             label4.TabIndex = 6;
             label4.Text = "File Size";
             // 
             // fileSizeTextBox
             // 
-            fileSizeTextBox.Location = new Point(12, 88);
+            fileSizeTextBox.Location = new Point(14, 111);
+            fileSizeTextBox.Margin = new Padding(3, 4, 3, 4);
             fileSizeTextBox.Name = "fileSizeTextBox";
-            fileSizeTextBox.Size = new Size(100, 23);
+            fileSizeTextBox.Size = new Size(114, 27);
             fileSizeTextBox.TabIndex = 7;
             // 
             // CalculateButton
             // 
-            CalculateButton.Location = new Point(12, 117);
+            CalculateButton.Location = new Point(14, 143);
+            CalculateButton.Margin = new Padding(3, 4, 3, 4);
             CalculateButton.Name = "CalculateButton";
-            CalculateButton.Size = new Size(75, 36);
+            CalculateButton.Size = new Size(114, 69);
             CalculateButton.TabIndex = 8;
             CalculateButton.Text = "Calculate";
             CalculateButton.UseVisualStyleBackColor = true;
             CalculateButton.Click += CalculateButton_Click;
             // 
-            // textBox1
+            // summaryTextbox
             // 
-            textBox1.Location = new Point(118, 88);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(206, 23);
-            textBox1.TabIndex = 9;
+            summaryTextbox.Location = new Point(134, 100);
+            summaryTextbox.Margin = new Padding(3, 4, 3, 4);
+            summaryTextbox.Multiline = true;
+            summaryTextbox.Name = "summaryTextbox";
+            summaryTextbox.Size = new Size(235, 148);
+            summaryTextbox.TabIndex = 9;
             // 
-            // progressBar1
+            // StartBenchmarkButton
             // 
-            progressBar1.Location = new Point(118, 117);
-            progressBar1.Name = "progressBar1";
-            progressBar1.Size = new Size(206, 36);
-            progressBar1.TabIndex = 10;
+            StartBenchmarkButton.Location = new Point(14, 255);
+            StartBenchmarkButton.Name = "StartBenchmarkButton";
+            StartBenchmarkButton.Size = new Size(235, 67);
+            StartBenchmarkButton.TabIndex = 11;
+            StartBenchmarkButton.Text = "Start Factorial Benchmark";
+            StartBenchmarkButton.UseVisualStyleBackColor = true;
+            StartBenchmarkButton.Click += StartBenchmarkButton_Click;
             // 
-            // Form1
+            // resultTextbox
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            resultTextbox.FormattingEnabled = true;
+            resultTextbox.ItemHeight = 20;
+            resultTextbox.Location = new Point(14, 328);
+            resultTextbox.Name = "resultTextbox";
+            resultTextbox.ScrollAlwaysVisible = true;
+            resultTextbox.Size = new Size(356, 324);
+            resultTextbox.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(255, 295);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(114, 27);
+            textBox2.TabIndex = 14;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(255, 252);
+            label5.Name = "label5";
+            label5.Size = new Size(108, 20);
+            label5.TabIndex = 15;
+            label5.Text = "Average score";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(255, 272);
+            label6.Name = "label6";
+            label6.Size = new Size(107, 20);
+            label6.TabIndex = 16;
+            label6.Text = "Lower is better";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(135, 76);
+            label7.Name = "label7";
+            label7.Size = new Size(71, 20);
+            label7.TabIndex = 17;
+            label7.Text = "Summary";
+            // 
+            // comboBox1
+            // 
+            comboBox1.BackColor = SystemColors.ControlLightLight;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "File Size GB", "File Size MB" });
+            comboBox1.Location = new Point(14, 76);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(114, 28);
+            comboBox1.TabIndex = 18;
+            // 
+            // S
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(338, 166);
-            Controls.Add(progressBar1);
-            Controls.Add(textBox1);
+            ClientSize = new Size(384, 667);
+            Controls.Add(comboBox1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(textBox2);
+            Controls.Add(resultTextbox);
+            Controls.Add(StartBenchmarkButton);
+            Controls.Add(summaryTextbox);
             Controls.Add(CalculateButton);
             Controls.Add(fileSizeTextBox);
             Controls.Add(label4);
@@ -145,7 +222,8 @@
             Controls.Add(providerTextBox);
             Controls.Add(latencyTextBox);
             Controls.Add(speedTextBox);
-            Name = "Form1";
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "S";
             Text = "Weeeee";
             ResumeLayout(false);
             PerformLayout();
@@ -162,7 +240,13 @@
         private Label label4;
         private TextBox fileSizeTextBox;
         private Button CalculateButton;
-        private TextBox textBox1;
-        private ProgressBar progressBar1;
+        private TextBox summaryTextbox;
+        private Button StartBenchmarkButton;
+        private ListBox resultTextbox;
+        private TextBox textBox2;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private ComboBox comboBox1;
     }
 }
--- Internet/Form1.cs
+++ Internet/Form1.cs
@@ -1,31 +1,187 @@
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Internet
 {
-    public partial class Form1 : Form
+    public partial class S : Form
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+        private List<(int coreIndex, double coreTime)> coreResults = new List<(int coreIndex, double coreTime)>();
+        private double averageScore = 0.0;
+        private readonly object lockObject = new object();
+        private bool isBenchmarking = false;
         private double speed;
         private double latency;
         private String provider;
+        double fileSize;
+        double downloadTime;
+        public S()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            comboBox1.SelectedIndex = 1;
+            summaryTextbox.ReadOnly = true;
+            textBox2.ReadOnly = true;
+        }
         private void CalculateButton_Click(object sender, EventArgs e)
         {
-            if (speedTextBox.Text == null) speed = 0;
-            else
-                speed = double.Parse(speedTextBox.Text);
-            if (latencyTextBox.Text == null) latency = 0;
-            else speed = double.Parse(latencyTextBox.Text);
-            if (providerTextBox.Text == null) provider = "Unknown";
-            else provider = providerTextBox.Text;
-            int iterations = 10000000;  // Adjust this value to control CPU load
+            summaryTextbox.Text = "";
 
-            for (int i = 0; i < iterations; i++)
-            {
-                double result = Math.Pow(2, i);
-            }
+            if (speedTextBox.Text == "") speed = 20;
+            else speed = double.Parse(speedTextBox.Text);
+
+            if (fileSizeTextBox.Text == "") fileSize = 2000;
+            else fileSize = double.Parse(fileSizeTextBox.Text);
+
+            if (latencyTextBox.Text == "") latency = 200;
+            else latency = double.Parse(latencyTextBox.Text);
+
+            if (providerTextBox.Text == "") provider = "Unknown";
+            else provider = providerTextBox.Text;
+
             Internet internet = new Internet(speed, latency, provider);
 
+            if (comboBox1.SelectedIndex == 1)
+            {
+                downloadTime = fileSize / (speed / 8);
+            }
+            else
+            {
+                downloadTime = (fileSize * 1000) / (speed / 8);
+            }
+
+            downloadTime = Math.Round((downloadTime / 60 / 60), 2);
+
+            if (speed < 6)
+            {
+                summaryTextbox.Text = $"Wow, you've got the world's slowest internet with {provider}! It'll take {downloadTime} hours to download the file with a whopping {speed} Mbps speed and {latency} ms latency.";
+            }
+            if (speed > 6 && speed < 12)
+            {
+                summaryTextbox.Text = $"Your {provider} internet is on a coffee break. It estimates just {downloadTime} hours to download the file with a speedy {speed} Mbps connection and {latency} ms latency.";
+            }
+            if (speed > 12 && speed < 20)
+            {
+                summaryTextbox.Text = $"Your {provider} internet moves at a glacial pace. Expect a mere {downloadTime} hours for the download with {speed} Mbps speed and {latency} ms latency.";
+            }
+            if (speed > 20 && speed < 34)
+            {
+                summaryTextbox.Text = $"You've got the patience of a saint with {provider}! It'll only be {downloadTime} hours to get that file, thanks to a decent {speed} Mbps speed and {latency} ms latency.";
+            }
+            if (speed > 34 && speed < 50)
+            {
+                summaryTextbox.Text = $"Your {provider} internet is making progress. It's forecasting {downloadTime} hours for the download with {speed} Mbps speed and {latency} ms latency.";
+            }
+            if (speed > 50 && speed < 75)
+            {
+                summaryTextbox.Text = $"Not too shabby with {provider}! It'll take just {downloadTime} hour to snatch the file, thanks to a respectable {speed} Mbps speed and {latency} ms latency.";
+            }
+            if (speed > 75 && speed < 125)
+            {
+                summaryTextbox.Text = $"Your {provider} internet is above average. It'll be done in {downloadTime * 60} minutes with {speed} Mbps speed and {latency} ms latency.";
+            }
+            if (speed > 125 && speed < 250)
+            {
+                summaryTextbox.Text = $"Decent speed with {provider}! It estimates a quick {downloadTime * 60} minutes for the download with {speed} Mbps speed and {latency} ms latency.";
+            }
+            if (speed > 250 && speed < 500)
+            {
+                summaryTextbox.Text = $"Impressive with {provider}! Your internet will get the file in {downloadTime * 60} minutes flat with a speedy {speed} Mbps connection and {latency} ms latency.";
+            }
+            if (speed > 500)
+            {
+                summaryTextbox.Text = $"You've got the Usain Bolt of internet connections with {provider}. It'll be lightning-fast, just {downloadTime * 60} minutes to go with an incredible {speed} Mbps speed and {latency} ms latency.";
+            }
+
         }
+
+
+        private async void StartBenchmarkButton_Click(object sender, EventArgs e)
+        {
+            coreResults.Clear();
+            averageScore = 0.0;
+
+            if (isBenchmarking)
+            {
+                MessageBox.Show("Benchmark is already running.");
+                return;
+            }
+
+            isBenchmarking = true;
+            StartBenchmarkButton.Enabled = false;
+            int numCores = Environment.ProcessorCount;
+            textBox2.Text = "";
+            resultTextbox.Items.Clear();
+
+            await Task.Run(() =>
+            {
+                int NumIterations = 13000;
+
+                Parallel.For(0, numCores, coreIndex =>
+                {
+                    var sw = new Stopwatch();
+                    sw.Start();
+
+                    for (int i = 0; i < NumIterations; i++)
+                    {
+                        EngageCore(coreIndex, NumIterations);
+                    }
+
+                    sw.Stop();
+                    double coreTime = sw.Elapsed.TotalMilliseconds / NumIterations;
+
+                    lock (lockObject)
+                    {
+                        averageScore += coreTime;
+                    }
+                    coreResults.Add((coreIndex, coreTime * 1000));
+                });
+            });
+
+            coreResults = coreResults.OrderBy(result => result.coreIndex).ToList();
+
+            StartBenchmarkButton.Enabled = true;
+            isBenchmarking = false;
+
+            foreach (var result in coreResults)
+            {
+                resultTextbox.Items.Add($"Core {result.coreIndex}: {(int)result.coreTime} Microseconds per iteration");
+            }
+
+            double average = averageScore / numCores;
+            textBox2.Text = (((int)(average * 1000))).ToString();
+        }
+
+        private void EngageCore(int coreIndex, int NumIterations)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < NumIterations; i++)
+            {
+                int n = random.Next(1, 10);
+                long result = CalculateFactorial(n);
+            }
+        }
+
+        private long CalculateFactorial(int n)
+        {
+            if (n == 0)
+                return 1;
+
+            long result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+
     }
 }
